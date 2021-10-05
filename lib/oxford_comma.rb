636fab983cc2a50d54a4fc46b1829d330719fca7
itsmattpaw <itsmattpaw@gmail.com>
2021-10-05 08:07:16 -0400

    commit tree a6836af6afaa89a6abc6874646d8bea3d90e8cfa

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    case array.count
+    when 1
+        return array.join
+    when 2
+        last_element = array.last #save last element
+        array[array.count - 1] = "and" #replace last with and
+        array.join(" ") << " " << last_element #convert to string without commas
+    else
+        last_element = array.last #save last element
+        array[array.count - 1] = "and" #replace last with and
+        array.join(", ") << " " << last_element #convert to string with commas
+    end
 end
